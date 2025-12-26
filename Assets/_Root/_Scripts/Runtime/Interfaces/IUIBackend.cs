a046bf82540cb9f8ce6af348c1f7d81001202fcb
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2025-12-26 19:05:23 +0000

    Created a UI Backend Interface.
    - IUIBackend class defines functions for Init(), Shutdown(), and OnDeviceChange().

---
+++ Assets/_Root/_Scripts/Runtime/Interfaces/IUIBackend.cs
@@ -0,0 +1,11 @@
+﻿using DeviceType = PROJECTNAME.Managers.DeviceType;
+
+namespace PROJECTNAME.Interfaces
+{
+public interface IUIBackend
+{
+	void Init();
+	void Shutdown();
+	void OnDeviceChanged(DeviceType deviceType);
+}
+}
